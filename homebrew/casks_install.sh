228d68787564d045c80749895fc52a55cc03b4d3
AndrewBell <andrew@recursivechaos.com>
2020-10-11 18:12:34 -0500

    Add homebrews

---
+++ homebrew/casks_install.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+# Installs Homebrew Casks.
+
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install docker
+brew cask install jetbrains-toolbox
+brew cask install atom
+brew cask install slack
---
+++ homebrew/formulas_install.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+brew update
+
+brew install nvm
+brew install docker
+brew install mas
+brew install adr-tools
+brew install terraform
+brew install pyenv
+brew install fortune
